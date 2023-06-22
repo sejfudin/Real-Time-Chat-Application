846c90ab81821a9e6e627a4f5a42cf8d1f357c8e
Sejfudin <sejfudindu@maestralsolutions.com>
2023-06-23 01:00:36 +0200

    user, chat and message models added

---
+++ api/models/chat.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create chat schema
+const chatSchema = new Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolien, default: false },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Message',
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Chat', chatSchema);
