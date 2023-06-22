bdf7f861e781abb5fa1bd88ea5d3608127538009
Sejfudin <sejfudindu@maestralsolutions.com>
2023-06-23 00:29:22 +0200

    simple endpoints with dummy data

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const chats = require('./dummy-data/data');
 
 const app = express();
 dotenv.config();
+
+app.get('/chat', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/chat/:id', (req, res) => {
+  console.log(req.params.id);
+  const { id } = req.params;
+  const singleChat = chats.find((c) => c._id === id);
+  res.send(singleChat);
+});
 
 const port = process.env.PORT || 5000;
 
