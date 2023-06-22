94e45dfefcc520a1de1d5ccd235652d4c04b2600
Sejfudin <sejfudindu@maestralsolutions.com>
2023-06-22 23:53:48 +0200

    SOme dummy data added

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express();
-const PORT = 5000;
+dotenv.config();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
