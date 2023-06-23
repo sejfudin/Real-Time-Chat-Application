e95fdaeb9c893d01048b4d4357c54466e8721953
Sejfudin <sejfudindu@maestralsolutions.com>
2023-06-24 00:26:39 +0200

    register and login user endpoints implemented

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   {
