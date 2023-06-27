4d826364da9d76cf1d824efafd177d9a9ffbba49
Sejfudin <sejfudindu@maestralsolutions.com>
2023-06-27 16:45:48 +0200

    Controllers refactored, logic moves to services

--- api/controllers/messageControllers.js
+++ api/controllers/messageControllers.js
@@ -1,7 +1,9 @@
 const Message = require('../models/messageModel');
 const User = require('../models/userModel');
 const Chat = require('../models/chatModel');
+const messageService = require('../services/messageService');
 
+//Send a message
 const sendMessage = async (req, res) => {
   const { content, chatId } = req.body;
 
@@ -9,35 +11,20 @@ const sendMessage = async (req, res) => {
     console.log('Invalid data passed into request');
     return res.sendStatus(400);
   }
-  var newMessage = {
-    sender: req.user._id,
-    content,
-    chat: chatId,
-  };
 
   try {
-    var message = await Message.create(newMessage);
-    message = await message.populate('sender', 'name');
-    message = await message.populate('chat');
-    message = await User.populate(message, {
-      path: 'chat.users',
-      select: 'name email',
-    });
-
-    await Chat.findByIdAndUpdate(req.body.chatId, {
-      latestMessage: message,
-    });
+    const message = await messageService.sendMessage(req.user._id, content, chatId);
     res.json(message);
   } catch (error) {
     res.status(400);
     throw new Error(error.message);
   }
 };
+
+//Get all messages
 const allMessages = async (req, res) => {
   try {
-    const messages = await Message.find({ chat: req.params.chatId })
-      .populate('sender', 'name email')
-      .populate('chat');
+    const messages = await messageService.getAllMessages(req.params.chatId);
     res.json(messages);
   } catch (error) {
     res.status(400);
---
+++ api/services/userService.js
@@ -0,0 +1,47 @@
+const { passwordCrypt } = require('../config/password');
+const User = require('../models/userModel');
+
+const registerUser = async (name, email, password) => {
+  if (!name || !email || !password) {
+    return null;
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return null;
+  }
+
+  const hashedPassword = await passwordCrypt(password);
+
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  return await user.save();
+};
+
+const loginUser = async (email, password) => {
+  if (!email || !password) {
+    return null;
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && matchPassword(user.password, password)) {
+    return user;
+  }
+
+  return null;
+};
+
+const getAllUsers = async (keyword, loggedInUserId) => {
+  return await User.find(keyword).find({ _id: { $ne: loggedInUserId } });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getAllUsers,
+};
