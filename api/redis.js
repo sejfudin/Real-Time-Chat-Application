ced64ba22c662b571baa18eb74ce7af3bd4effaf
Sejfudin <sejfudindu@maestralsolutions.com>
2023-06-30 03:58:31 +0200

    redis reconfigured

--- api/redis.js
+++ api/redis.js
@@ -1,11 +1,5 @@
 const Redis = require('ioredis');
 
-const redisClient = new Redis({
-  host: 'red-cif2uctgkuvq1o3o1u0g',
-  port: 6379,
-  tls: {
-    rejectUnauthorized: false, // Set to true if using a valid SSL certificate
-  },
-});
+const redisClient = new Redis(process.env.REDIS_URL);
 
 module.exports = redisClient;
