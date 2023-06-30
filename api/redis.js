82cb53c885c878732b3f65cad47da02dfc61bfc7
Sejfudin <sejfudindu@maestralsolutions.com>
2023-06-30 03:47:00 +0200

    redis tls configured

--- api/redis.js
+++ api/redis.js
@@ -3,6 +3,9 @@ const Redis = require('ioredis');
 const redisClient = new Redis({
   host: 'red-cif2uctgkuvq1o3o1u0g',
   port: 6379,
+  tls: {
+    rejectUnauthorized: false, // Set to true if using a valid SSL certificate
+  },
 });
 
 module.exports = redisClient;
